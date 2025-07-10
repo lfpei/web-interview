720d5dd131535300ef783d6216634b384f8bb321
lon <lf_pei@163.com>
2025-07-10 18:14:40 +0800

    feat: 调度器题目修改

--- js/场景题/scheduler.js
+++ js/场景题/scheduler.js
@@ -3,48 +3,40 @@
  *  编写一个任务调度器正确执行下面的用例代码。
  */
 class Scheduler {
-  constructor() {
+  constructor(maxLimit = 2) {
     this.queue = [];
-    this.maxLimit = 2;
+    this.maxLimit = maxLimit;
     this.runningNums = 0;
   }
-  addTask(task) {
-    this.queue.push(() => new Promise((resolve, reject) => task().then(resolve, reject)));
-    this._run();
+  add(time, name) {
+    this.queue.push(() => new Promise(resolve => setTimeout(resolve, time)).then(() => console.log(name)));
   }
-  _run() {
+  run() {
     if(this.runningNums >= this.maxLimit || this.queue.length === 0) {
       return;
     }
-    // console.log('runningNums:', this.runningNums, 'queue length:', this.queue.length);
     const task = this.queue.shift();
     this.runningNums++;
-    task().finally(()=>{
+    task().finally(() => {
       this.runningNums--;
-      this._run();
+      this.run();
     })
+  }
+  start() {
+    for(let i = 0; i < this.maxLimit; i++) {
+      this.run();
+    }
   }
 }
 
 // 用例代码
-// 辅助函数
-const promiseTimeout = (time, value) => {
-  console.time('promiseTimeout' + value);
-  return () => new Promise(resolve => {
-    setTimeout(() => {
-      resolve(value);
-      console.timeEnd('promiseTimeout' + value);
-    }, time);
-  })
-}
 const scheduler = new Scheduler();
-scheduler.addTask(promiseTimeout(1000, '1'));
-scheduler.addTask(promiseTimeout(500, '2'));
-scheduler.addTask(promiseTimeout(300, '3'));
-scheduler.addTask(promiseTimeout(400, '4'));
-// output: 2 3 1 4
-// 一开始，1、2两个任务进入队列(队列最大长度为2)
-// 500ms时，2完成，输出2，任务3进队
-// 800ms时，3完成，输出3，任务4进队
-// 1000ms时，1完成，输出1
-// 1200ms时，4完成，输出4
+const addTask = (time, name) => {
+  scheduler.add(time, name);
+};
+addTask(1000,"1"); // 1000ms后输出1
+addTask(500,"2");  // 500ms后输出2
+addTask(600,"3"); // 1100ms后输出3
+addTask(400,"4"); // 1400ms后输出4
+scheduler.start();
+// output: 2 1 3 4
